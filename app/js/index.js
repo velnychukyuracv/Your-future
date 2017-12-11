cc8278159cecb256f54b2d7d05330a7cc67bc1e8
VelnychukYura <velnychukyura@gmail.com>
2017-12-11 14:49:24 +0200

    create project

---
+++ app/js/index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+//Smooth scroll
+$(document).ready(function(){
+    $("#header").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+
+//slick slider
+//slick top slider
+$('.sl-main').slick({
+        autoplay: true,
+        autoplaySpeed:3000,
+        dots:true
+    }
+);
+//slick bottom slider
+$('.sl').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed:3000,
+       dots:true
+});
+
+
+//Zoom-gallery pop up
+$(document).ready(function() {
+    $('.zoom-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true,
+            titleSrc: function(item) {
+                return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+            }
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+
+    });
+});
