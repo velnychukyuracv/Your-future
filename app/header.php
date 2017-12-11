800172d79238d2813dfdec8b21eb8bed66dc6eaf
VelnychukYura <velnychukyura@gmail.com>
2017-12-11 16:29:21 +0200

    rewrite index.html,  create second page and include php file

---
+++ app/header.php
@@ -0,0 +1,48 @@
+<?php
+echo '
+<header>
+  <!--top logo section start-->
+    <div class="topHeader">
+        <div class="leftHeader">
+            <a  href="#" class="logoDesktop"><img class="img-responsive" src="img/logo.png" alt="Logo"></a>
+            <h2>благодійна організація <br> міжнародний благодійний фонд "твоє майбутнє" </h2>
+        </div>
+
+        <div class="rightHeader">
+            <a href="#" type="button" class="help">допомогти</a>
+            <h2>Зв'яжіться з нами <br> <span>+38 (067) 46-90-571</span> </h2>
+        </div>
+    </div>
+  <!--top logo section end-->
+
+  <!--navigation section start-->
+     <nav>
+          <ul class="b-nav">
+              <li>
+                  <a class="b-link" href="#">Про нас </a>
+                  <ul class="b-nav">
+                      <li><a class="b-link" href="#" >Подяки </a></li>
+                      <li><a class="b-link" href="#" >Вакансії </a></li>
+                      <li><a class="b-link" href="#" >Контакти</a></li>
+                  </ul>
+              </li>
+              <li><a class="b-link" href="#">Проекти </a></li>
+              <li>
+                  <a class="b-link " href="#">Звітність</a>
+                  <ul class="b-nav">
+                      <li><a class="b-link" href="#" >Відео </a></li>
+                      <li><a class="b-link" href="#" >Фотозвітність </a></li>
+                      <li><a class="b-link" href="#" >Квартальна звітність</a></li>
+                      <li><a class="b-link" href="#" >Фінансова звітність</a></li>
+                  </ul>
+              </li>
+              <li><a class="b-link" href="#" >Документацiя</a></li>
+              <li><a class="b-link" href="#" >Співпраця</a></li>
+              <li><a class="b-link" href="#" >Реквізити</a></li>
+              <li><a class="b-link" href="#" >Потрібна допомога?</a></li>
+          </ul>
+     </nav>
+  <!--navigation section end-->
+</header>
+<!--Header section end-->';
+?>
