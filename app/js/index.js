91edd3403279826871b906e290e6cad94354226d
VelnychukYura <velnychukyura@gmail.com>
2017-12-13 03:11:04 +0200

    new file

--- app/js/index.js
+++ app/js/index.js
@@ -51,3 +51,12 @@ $(document).ready(function() {
     });
 });
 
+//slick bottom slider
+$('.sl_thanksgiving').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed:3000,
+    dots:true
+});
