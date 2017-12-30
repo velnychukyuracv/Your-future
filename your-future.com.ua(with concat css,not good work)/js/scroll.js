ac19bab792f3bac7d50d2ae2f4f9f29c2b0461c8
VelnychukYura <velnychukyura@gmail.com>
2017-12-30 16:08:40 +0200

    concat test

---
+++ your-future.com.ua(with concat css,not good work)/js/scroll.js
@@ -0,0 +1,10 @@
+
+
+// Scroll Events
+$(window).scroll(function(){
+    if($(window).scrollTop() ) {
+        $('#header').css({ position:'fixed'});
+    } else{
+        $('#header').css({ position:'relative'});
+    }
+});
---
+++ your-future.com.ua(with concat css,not good work)/requisites.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Реквізити | Твоє майбутнє</title>
+
+    <!--Favicon logo-->
+    <link rel="shortcut icon" href="./img/logo.png" type="image/png">
+
+    <!--Font-Google-->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:500,700" rel="stylesheet">
+
+    <!-- Magnific Popup core CSS file -->
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css'>
+
+      <!--Style-->
+        <link href="css/style.css" rel="stylesheet">
+
+
+</head>
+<body>
+
+<!--Navigation out menu section start-->
+<? require_once('navigation_out_menu.php') ?>
+<!--Navigation out menu section end-->
+
+<!--Header section start-->
+<? require_once('header.php') ?>
+<!--Header section end-->
+
+<!--Main section start-->
+<main id="mainContainer">
+    <h4>Реквізити: </h4>
+    <br>
+    <p>Рахунок для валюти: UAH</p>
+    <p>Благодійна організація «МБФ «ТВОЄ МАЙБУТНЄ», м.Київ, р/р 26004559823; ЄДРПОУ 41155236; МФО банку 380805;ПАТ «Райффайзен Банк Аваль»</p>
+    <br>
+    <p>Картковий рахунок: UAH</p>
+    <p>4188 3730 2777 7561 Директор БО «МБФ «ТВОЄ МАЙБУТНЄ» Tereshchenko Bogdan Andreevich АТ «Райффайзен банк Аваль»</p>
+    <br>
+    <p>Рахунок для валюти: USD</p>
+    <p>СHARITY ORGANIZATION “INTERNATIONAL CHARITY FOUNDATION “YOUR FUTURE”</p>
+    <p>Банк Бенефіціар: Raiffeisen Bank Aval, Kiev, Ukraine</p>
+    <p>26003568246</p>
+    <p>SWIFT code: AVALUAUK</p>
+    <p>Пизначення платежу: Donation</p>
+    <a href="help-pay.php" class="help">допомогти</a>
+</main>
+<!--Main section end-->
+
+
+<!--Footer section start-->
+<? require_once('footer.php') ?>
+<!--Footer section end-->
+
+<!--scripts-->
+
+    <!--Font-Awesome-->
+    <script  src="https://use.fontawesome.com/2ce0afabeb.js"></script>
+
+    <!--jquery-->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+    <!-- Magnific Popup core JS file -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js'></script>
+
+    <!--js-->
+    <script  src="js/index.js"></script>
+    <script  src="js/scroll.js"></script>
+    <script  src="js/effects_button_help.js"></script>
+
+</body>
+</html>
